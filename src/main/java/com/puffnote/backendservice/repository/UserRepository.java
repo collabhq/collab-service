b09b1ad8350011b085ba413e9354d7a051452155
Karthik Narumanchi <karthiknr@gmail.com>
2019-03-16 11:24:11 +0100

    Added Javadoc to various classes

--- src/main/java/com/puffnote/backendservice/repository/UserRepository.java
+++ src/main/java/com/puffnote/backendservice/repository/UserRepository.java
@@ -7,9 +7,24 @@ import org.springframework.data.rest.core.annotation.RestResource;
 /**
  * Created by karthik on 2019-03-11
  */
+
+/**
+ * Data repository class used to implement data access on User objects
+ */
 @RestResource(exported = false)
 public interface UserRepository extends MongoRepository<User,String> {
+    /**
+     * Find user object by name
+     * @param name
+     * @return User object found
+     */
     public User findByName(String name);
+
+    /**
+     * Find User object by uuid
+     * @param uuid
+     * @return User object found
+     */
     public User findByUuid(String uuid);
 }
 
--- src/main/java/com/puffnote/backendservice/service/RoomService.java
+++ src/main/java/com/puffnote/backendservice/service/RoomService.java
@@ -7,19 +7,103 @@ import com.puffnote.backendservice.model.User;
  * Created by karthik on 2019-03-11
  */
 public interface RoomService {
+    /**
+     * List all Room objects
+     * @return All Room objects
+     */
     Iterable listAll();
+
+    /**
+     * Find Room object by id
+     * @param id
+     * @return Room object found
+     */
     Room findById(String id);
+
+    /**
+     * Find Room object by uuid
+     * @param uuid
+     * @return Room object found
+     */
     Room findByUuid(String uuid);
+
+    /**
+     * Find Room object by name
+     * @param name
+     * @return Room object found
+     */
     Room findByName(String name);
+
+    /**
+     * Save or Update Room object
+     * @param room
+     * @return Room object saved or updated
+     */
     Room saveOrUpdate(Room room);
+
+    /**
+     * Delete Room object
+     * @param room
+     */
     void delete(Room room);
+
+    /**
+     * Delete Room object by id
+     * @param id
+     */
     void deleteById(String id);
+
+    /**
+     * Check if Room object exists by id
+     * @param id
+     * @return
+     */
     boolean existsById(String id);
+
+    /**
+     * Delete all Room objects
+     */
     void deleteAll();
+
+    /**
+     * Add User object reference to Room object
+     * @param room
+     * @param user
+     */
     void addUserToRoom(Room room, User user);
+
+    /**
+     * Add User object reference to Room object by id
+     * @param roomId
+     * @param userId
+     */
     void addUserToRoomById(String roomId, String userId);
+
+    /**
+     * Add User object reference to Room object by uuid
+     * @param roomUuid
+     * @param userUuid
+     */
     void addUserToRoomByUuid(String roomUuid, String userUuid);
+
+    /**
+     * Remove User object reference from Room object
+     * @param room
+     * @param user
+     */
     void removeUserFromRoom(Room room, User user);
+
+    /**
+     * Remove User object reference from Room object by id
+     * @param roomId
+     * @param userId
+     */
     void removeUserFromRoomById(String roomId, String userId);
+
+    /**
+     * Remove User object reference from Room object by uuid
+     * @param roomUuid
+     * @param userUuid
+     */
     void removeUserFromRoomByUuid(String roomUuid, String userUuid);
 }
