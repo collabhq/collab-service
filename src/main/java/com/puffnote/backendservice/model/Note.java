7fbf4df74535c7d50c3714d6db680f400ba4363b
Karthik Narumanchi <karthiknr@gmail.com>
2019-03-16 11:23:24 +0100

    Modified Note object - Type and Value attributes
    Note object does not have a type variable anymore. Also changed value
    to a single String instead of list of Strings.
    
    Added Javadoc where needed.

--- src/main/java/com/puffnote/backendservice/model/Note.java
+++ src/main/java/com/puffnote/backendservice/model/Note.java
@@ -1,17 +1,20 @@
 package com.puffnote.backendservice.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.puffnote.backendservice.util.Constants;
 import com.puffnote.backendservice.util.CustomUUIDGenerator;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by karthik on 2019-03-11
+ */
+
+/**
+ * Class that represents a note object
  */
 @JsonIgnoreProperties({"id"})
 @Document(collection = "notes")
@@ -22,30 +25,27 @@ public class Note {
 
     private String uuid;//Use separate UUID for reference
     private String name;
-    private List<String> value;//List of strings determined based on type of note-text or list
-    private String type;
+    private String value;//String containing markdown text
 
     /**
-     * No-arg Constructor - Enforce UUID generation
+     * No-arg Constructor
+     * UUID enforced
      */
     public Note() {
         this.uuid = CustomUUIDGenerator.generateRandomUUID();
         this.name = "";
-        this.value = new ArrayList<String>();
-        this.type = "";
+        this.value = "";
     }
 
     /**
      * Constructor
      * @param name
      * @param value
-     * @param type
      */
-    public Note(String name, List<String> value, String type) {
+    public Note(String name, String value) {
         this.uuid = CustomUUIDGenerator.generateRandomUUID();
         this.name = name;
         this.value = value;
-        this.type = type;
     }
 
     /**
@@ -64,32 +64,43 @@ public class Note {
         return this.uuid;
     }
 
+    /**
+     * Get name of note
+     * @return name
+     */
     public String getName() {
         return this.name;
     }
+
+    /**
+     * Set name for note
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
-    public List<String> getValue() {
+    /**
+     * Get value of text given to note
+     * @return value
+     */
+    public String getValue() {
         return this.value;
     }
-    public void setValue(List<String> value) {
-        this.value = value;
-    }
 
-    public String getType() {
-        return this.type;
-    }
-    public void setType(String type) {
-        this.type = type;
+    /**
+     * Set the value of text for note
+     * @param value
+     */
+    public void setValue(String value) {
+        this.value = value;
     }
 
     @Override
     public String toString() {
         return String.format(
-                "Note[id=%s, uuid=%s, name='%s', value='%s', type='%s]",
-                id, uuid, name, value, type);
+                "Note[id=%s, uuid=%s, name='%s', value='%s']",
+                id, uuid, name, value);
     }
 
 }
