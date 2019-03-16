0e74c171ba1c3bf070086ee3c97e05f376a2c510
Karthik Narumanchi <karthiknr@gmail.com>
2019-03-16 11:21:07 +0100

    Added short alphanumeric id creation for Room uuid
    Room uuid’s will now make use of 6 digit alphanumeric ids created using
    Apache commons RandomStringGenerator.
    
    Also added Javadoc where needed.

--- src/main/java/com/puffnote/backendservice/model/Room.java
+++ src/main/java/com/puffnote/backendservice/model/Room.java
@@ -3,16 +3,17 @@ package com.puffnote.backendservice.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.puffnote.backendservice.util.CustomUUIDGenerator;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by karthik on 2019-03-11
+ */
+
+/**
+ * Class that represents a room object
  */
 @JsonIgnoreProperties({"id", "userReferences"})
 @Document(collection = "rooms")
@@ -23,24 +24,25 @@ public class Room {
     private String uuid;//Use separate UUID for reference
     private String name;
 
-    //@DBRef
     private List<String> userReferences;//Use manual references to users stored in their collection
 
     /**
-     * No-arg Constructor - Enforce UUID generation
+     * No-arg Constructor
+     * UUID enforced and uses a short ID format
      */
     public Room() {
-        this.uuid = CustomUUIDGenerator.generateRandomUUID();
+        this.uuid = CustomUUIDGenerator.generateShortUUID();
         this.name = "";
         this.userReferences = new ArrayList<String>();
     }
 
     /**
      * Constructor
-     * @param name
+     * UUID enforced and uses a short ID format
+     * @param name Name of room
      */
     public Room(String name) {
-        this.uuid = CustomUUIDGenerator.generateRandomUUID();
+        this.uuid = CustomUUIDGenerator.generateShortUUID();
         this.name = name;
         this.userReferences = new ArrayList<String>();
     }
@@ -61,13 +63,26 @@ public class Room {
         return this.uuid;
     }
 
+    /**
+     * Get name of room
+     * @return name
+     */
     public String getName() {
         return this.name;
     }
+
+    /**
+     * Set name for room
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Get a list of DB references of all users linked to this room
+     * @return List of references
+     */
     public List<String> getUserReferences() {
         return this.userReferences;
     }
