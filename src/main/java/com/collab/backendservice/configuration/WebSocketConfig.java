85f3ef37085af3abfe2cc0960ef22835bfcb0fe6
Karthik Narumanchi <karthiknr@gmail.com>
2019-04-28 21:45:02 +0200

    Remove deprecated class - revert to interface

--- src/main/java/com/collab/backendservice/configuration/WebSocketConfig.java
+++ src/main/java/com/collab/backendservice/configuration/WebSocketConfig.java
@@ -2,19 +2,16 @@ package com.collab.backendservice.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-
-
-import static org.springframework.messaging.simp.SimpMessageType.*;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 /**
  * Created by sudeshgutta on 2019-03-12
  */
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     /**
      * Configures the STOMP web socket endpoint of service
